e339d7e115cbd5edd95fb14ad3de4cbb1c561188
Ryan Remaly <remalyryan@gmail.com>
2021-01-27 07:17:56 -0500

    Changed some classes around and added a few more challenges

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,20 @@ public class Main {
         Regex matchingSpecificCharacter = new Regex("matchingSpecificCharacter.txt", "^[1|2|3][1|2|0][x|s|0][3|0|A|a][x|s|u][\\,|\\.]$");
         matchingSpecificCharacter.regexCheck();
 
+
+        // ^ inside brackets is like NOT
         String exSpecChar = "^\\D[^aeiou][^bcDF][^\\s][^AEIOU][^\\.\\,]$";
         Regex excludingSpecificCharacters = new Regex("matchingSpecificCharacter.txt", exSpecChar);
 
+        String matchingCharacterRange = "^[a-z][1-9][^a-z][^A-Z][A-Z]";
 
+        //https://www.hackerrank.com/challenges/matching-one-or-more-repititions/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+        String oneOrMoreRepetitions = "^\\d+[A-Z]+[a-z]+$";
 
+        // https://www.hackerrank.com/challenges/matching-ending-items/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+        String matchingEndingItems = "^[a-zA-Z]*s$";
+
+        // https://www.hackerrank.com/challenges/backreferences-to-failed-groups/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+        String backReferencesToFailedGroups =
     }
 }
